f693912d3171d6636265b58ac57d4bdecc499223
Dai Li <d.li@simplylive.tv>
2021-05-11 16:28:46 +0800

    add log

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,7 @@
-#define _CRT_SECURE_NO_WARNINGS 
+#define _CRT_SECURE_NO_WARNINGS
 #include "..\inc\Camera.h"
 #include <Shlwapi.h>
-#pragma comment(lib,"Shlwapi.lib")
+#pragma comment(lib, "Shlwapi.lib")
 
 #define SPDLOG_ACTIVE_LEVEL 0
 #include <spdlog/spdlog.h>
@@ -12,13 +12,13 @@ inline void initLogger(const char *folderPath)
 {
 	if (folderPath == nullptr)
 		folderPath = ".\\";
-	auto pCreateLogger = [&](std::string loggerName,bool bDefault=false) {
-
+	auto pCreateLogger = [&](std::string loggerName, bool bDefault = false)
+	{
 		auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
 		console_sink->set_level(spdlog::level::trace);
 
-		std::string fileName = std::string(folderPath)+"/logs/" + loggerName + ".txt";
-		auto file_sink = std::make_shared<spdlog::sinks::daily_file_sink_mt>(fileName, 0, 0,false,10);
+		std::string fileName = std::string(folderPath) + "/logs/" + loggerName + ".txt";
+		auto file_sink = std::make_shared<spdlog::sinks::daily_file_sink_mt>(fileName, 0, 0, false, 10);
 		file_sink->set_level(spdlog::level::trace);
 
 		std::vector<spdlog::sink_ptr> sinks;
@@ -43,9 +43,8 @@ inline void initLogger(const char *folderPath)
 	spdlog::set_pattern("[%x %X] %L %t [%s:%#-%!]>> %v");
 	spdlog::flush_every(std::chrono::seconds(5));
 	spdlog::flush_on(spdlog::level::info);
-
 }
-
+#include <filesystem>
 class Config
 {
 public:
@@ -58,22 +57,34 @@ public:
 		strcat(szBuf1, "\\sdkTool_Config.ini");
 		m_ConfigPathA = szBuf1;
 
+		std::error_code error;
+		if (!std::filesystem::exists(m_ConfigPathA, error)) //already have a file with the same name?
+		{
+			bWriteIni = true;
+		}
 		load();
 	}
-	
-	void load() {
+	bool isvalid = false;
+	bool load()
+	{
 		char szBuf1[MAX_PATH];
 		char szKey[MAX_PATH];
 		int nret = 0;
+		
+		bool isvalidTime = false;
 		for (int i = 0; i < 4; i++)
 		{
 			sprintf_s(szKey, "time%d", i + 1);
-			nret = getConfigString("main", szKey,szBuf1);
+			nret = getConfigString("main", szKey, szBuf1);
 			if (nret > 0)
 			{
 				std::string s(szBuf1);
-				auto pos1 = s.find(":");if(pos1 == std::string::npos)continue;
-				auto pos2 = s.find(":",pos1+1); if (pos2 == std::string::npos)continue;				
+				auto pos1 = s.find(":");
+				if (pos1 == std::string::npos)
+					continue;
+				auto pos2 = s.find(":", pos1 + 1);
+				if (pos2 == std::string::npos)
+					continue;
 				szBuf1[pos1] = '\0';
 				szBuf1[pos1] = '\0';
 				szBuf1[pos2] = '\0';
@@ -81,105 +92,127 @@ public:
 				cameraOBJ[i].workTime.tm_hour = atoi(szBuf1);
 				cameraOBJ[i].workTime.tm_min = atoi(szBuf1 + pos1 + 1);
 				cameraOBJ[i].workTime.tm_sec = atoi(szBuf1 + pos2 + 1);
-				
+				isvalidTime=true;
 			}
 		}
+		bool ipValid = false;
 		for (int i = 0; i < 4; i++)
 		{
 			sprintf_s(szKey, "ip_%d", i + 1);
 			nret = getConfigString("main", szKey, szBuf1);
 			if (nret > 0)
+			{
 				cameraOBJ[i].ip = szBuf1;
+				ipValid = true;
+			}
 		}
-
-		for (int i = 0; i < 3; i++)
+		for (int i = 0; i < 4; i++)
 		{
 			sprintf_s(szKey, "%d", i + 1);
-			cameraOBJ[i].param.AEMaxTime= getConfigInt(szKey, "AEMaxTime");
-			cameraOBJ[i].param.AVGLight = getConfigInt(szKey, "AVGLight" );
-			cameraOBJ[i].param.AGain =    getConfigInt(szKey, "AGain"    );
+			cameraOBJ[i].param.AEMaxTime = getConfigInt(szKey, "AEMaxTime");
+			cameraOBJ[i].param.AVGLight = getConfigInt(szKey, "AVGLight");
+			cameraOBJ[i].param.AGain = getConfigInt(szKey, "AGain");
 		}
+		
+		if (!isvalidTime)
+		{
+			SPDLOG_ERROR("read [main][timeX] error, no one was valid! exit");
+			return false;
+		}
+		if (!ipValid)
+		{
+			SPDLOG_ERROR("read [main][ip_X] error, no one was valid! exit");
+			return false;
+		}
+		isvalid = ipValid&& isvalidTime;
+		return true;
 	}
-	int getConfigInt(const char*szApp, const char*szKey)
-	{
-		if (bWriteIni)
-			WritePrivateProfileStringA(szApp,szKey,"",m_ConfigPathA.data());
-		return GetPrivateProfileIntA(szApp, szKey, 0, m_ConfigPathA.data());
-	}
-	int getConfigString(const char*szApp, const char*szKey,char*retVal)
+	int getConfigInt(const char *szApp, const char *szKey)
 	{
 		if (bWriteIni)
 			WritePrivateProfileStringA(szApp, szKey, "", m_ConfigPathA.data());
-		return  GetPrivateProfileStringA(szApp, szKey, "", retVal, MAX_PATH, m_ConfigPathA.data());
+		return GetPrivateProfileIntA(szApp, szKey, 0, m_ConfigPathA.data());
 	}
-	struct CameraOBJ {
+	int getConfigString(const char *szApp, const char *szKey, char *retVal)
+	{
+		if (bWriteIni)
+			WritePrivateProfileStringA(szApp, szKey, "", m_ConfigPathA.data());
+		return GetPrivateProfileStringA(szApp, szKey, "", retVal, MAX_PATH, m_ConfigPathA.data());
+	}
+	struct CameraOBJ
+	{
 		std::string ip;
-		bool isValid() {
+		bool isValid()
+		{
 			return !ip.empty() && workTime.isValid;
 		}
-		struct MyTM {
+		struct MyTM
+		{
 			bool isValid = false;
-			int tm_sec;   // seconds after the minute - [0, 60] including leap second
-			int tm_min;   // minutes after the hour - [0, 59]
-			int tm_hour;  // hours since midnight - [0, 23]
+			int tm_sec;	 // seconds after the minute - [0, 60] including leap second
+			int tm_min;	 // minutes after the hour - [0, 59]
+			int tm_hour; // hours since midnight - [0, 23]
 		};
-		struct MyTM workTime;		
+		struct MyTM workTime;
 		NET_DEV_CAMERAPARAM_V1 param;
 		uint64_t lastRunTime;
 	};
 	CameraOBJ cameraOBJ[4];
 	bool bWriteIni = false;
+
 private:
 	std::string m_ConfigPathA = ".\\sdkTool_Config.ini";
 };
 
-class Worker 
+class Worker
 {
 public:
 	void doWork()
 	{
 		for (int i = 0; i < 4; i++)
 		{
-			if (!config.cameraOBJ[i].isValid())continue;
-			
-			
+			if (!config.cameraOBJ[i].isValid())
+				continue;
+
 			struct tm local;
 			time_t t;
 			t = time(NULL);
-			localtime_s(&local, &t);			
-			bool bNeedRestart2 = (local.tm_hour == config.cameraOBJ[i].workTime.tm_hour 
-				&& local.tm_min == config.cameraOBJ[i].workTime.tm_min);
+			localtime_s(&local, &t);
+			bool bNeedRestart2 = (local.tm_hour == config.cameraOBJ[i].workTime.tm_hour && local.tm_min == config.cameraOBJ[i].workTime.tm_min);
 			if (bNeedRestart2 && (GetTickCount() - config.cameraOBJ[i].lastRunTime > 1000 * 60 * 2))
 			{
-				SPDLOG_INFO("{} setParam",i);
+				SPDLOG_INFO("{} setParam", i);
 				config.cameraOBJ[i].lastRunTime = GetTickCount();
 				CCamera camera;
 				strcpy(camera.m_ipaddrstr, config.cameraOBJ[i].ip.data());
 				camera.connect();
-				camera.set_3A_PARAM_V1(config.cameraOBJ[i].param);/**/
+				camera.set_3A_PARAM_V1(config.cameraOBJ[i].param); /**/
 			}
 		}
 	}
 	Config config;
-	void start(int argc) {
-		
+
+	void start(int argc)
+	{
+		if (!config.isvalid)
+			return;
 		if (argc > 1)
 			config.bWriteIni = true;
 
 		config.load();
-		auto a =std::thread([&]() {
-
-			while (1)
-			{
-				Sleep(1000);
-				doWork();
-			}
-			});
+		auto a = std::thread([&]()
+							 {
+								 while (1)
+								 {
+									 Sleep(1000);
+									 doWork();
+								 }
+							 });
 
 		a.join();
 	}
 };
-int main(int argc,char*argv[])
+int main(int argc, char *argv[])
 {
 	initLogger(nullptr);
 	printf("\n----------SDKTOOls  ----\n");
