2a3b2b2e5983ec3397dd68d8b5273a4e22dfb4c2
Dai Li <d.li@simplylive.tv>
2021-05-11 16:10:28 +0800

    update

--- Source.cpp
+++ Source.cpp
@@ -2,6 +2,50 @@
 #include "..\inc\Camera.h"
 #include <Shlwapi.h>
 #pragma comment(lib,"Shlwapi.lib")
+
+#define SPDLOG_ACTIVE_LEVEL 0
+#include <spdlog/spdlog.h>
+#include "spdlog/sinks/daily_file_sink.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/sinks/rotating_file_sink.h"
+inline void initLogger(const char *folderPath)
+{
+	if (folderPath == nullptr)
+		folderPath = ".\\";
+	auto pCreateLogger = [&](std::string loggerName,bool bDefault=false) {
+
+		auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+		console_sink->set_level(spdlog::level::trace);
+
+		std::string fileName = std::string(folderPath)+"/logs/" + loggerName + ".txt";
+		auto file_sink = std::make_shared<spdlog::sinks::daily_file_sink_mt>(fileName, 0, 0,false,10);
+		file_sink->set_level(spdlog::level::trace);
+
+		std::vector<spdlog::sink_ptr> sinks;
+		sinks.push_back(console_sink);
+		sinks.push_back(file_sink);
+
+		auto logger = std::make_shared<spdlog::logger>(loggerName, sinks.begin(), sinks.end());
+		//{ console_sink, file_sink }
+		//logger->flush_on(spdlog::level::info);
+		logger->set_level(spdlog::level::debug);
+		logger->set_pattern("[%x %X] %L %t [%s:%#-%!]>> %v");
+		logger->info("########################STARt##############################");
+
+		spdlog::register_logger(logger);
+		if (bDefault)
+			spdlog::set_default_logger(logger);
+		//logger->flush();
+	};
+	/**/
+
+	pCreateLogger("mainLog", true);
+	spdlog::set_pattern("[%x %X] %L %t [%s:%#-%!]>> %v");
+	spdlog::flush_every(std::chrono::seconds(5));
+	spdlog::flush_on(spdlog::level::info);
+
+}
+
 class Config
 {
 public:
@@ -107,6 +151,7 @@ public:
 				&& local.tm_min == config.cameraOBJ[i].workTime.tm_min);
 			if (bNeedRestart2 && (GetTickCount() - config.cameraOBJ[i].lastRunTime > 1000 * 60 * 2))
 			{
+				SPDLOG_INFO("{} setParam",i);
 				config.cameraOBJ[i].lastRunTime = GetTickCount();
 				CCamera camera;
 				strcpy(camera.m_ipaddrstr, config.cameraOBJ[i].ip.data());
@@ -136,6 +181,8 @@ public:
 };
 int main(int argc,char*argv[])
 {
+	initLogger(nullptr);
+	printf("\n----------SDKTOOls  ----\n");
 	Worker worker;
 	worker.start(argc);
 	return 0;
