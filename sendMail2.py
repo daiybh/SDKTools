d37326d05edcbf754b5aa10dc30ff5d66d273b71
Dai Li <d.li@simplylive.tv>
2021-11-23 13:52:04 +0800

    a

--- sendMail2.py
+++ sendMail2.py
@@ -46,7 +46,7 @@ z = zipfile.ZipFile(in_memory_zip,'w',zipfile.ZIP_DEFLATED)
 flist=[r'sdktools.pdb',r'sdktools.exe']
 for a in flist:
     z.write(r'out/build/x86-Release/'+a,a)
-
+z.write(r'openDump.bat','openDump.bat')
 z.close()
 
 
